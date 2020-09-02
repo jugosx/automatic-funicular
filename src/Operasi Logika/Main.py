b03cc7804ada60c0173e7b6a4d69d3631bb1dc11
jugosx <priskillacantix@gmail.com>
2020-09-02 08:05:42 +0700

    [Operasi Logika] NOT, OR

---
+++ src/Operasi Logika/Main.py
@@ -0,0 +1,30 @@
+#Operasi Logika atau Boolean
+
+#NOT, OR, AND, XOR
+
+#NOT
+print('====NOT====')
+a = False
+c = not a
+print('data a =', a)
+print('------------------ NOT')
+print('data c =', c)
+
+#OR
+print('====OR====')
+a = False
+b = False
+c = a or b
+print(a,'OR', b, '=', c)
+a = True
+b = False
+c = a or b
+print(a,' OR', b, '=', c)
+a = False
+b = True
+c = a or b
+print(a,'OR', b, ' =', c)
+a = True
+b = True
+c = a or b
+print(a,' OR', b, ' =', c)
